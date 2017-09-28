5a3cee37bb31dbffd179cef8c58706a7d36c6b0b
sandhyaRaniBogadi <sandhya.sandy731@gmail.com>
2017-09-28 12:18:01 +0530

    Add files via upload

---
+++ avn Tech/script.js
@@ -0,0 +1,10 @@
+funtion readmore()
+{
+	document.getElementById("heading").innerHtml()="Life Sciences and Biostatistics";
+	document.getElementById("accordion").innerHtml()=<p>PVR bring expertise and insights in designing trials and developing complex statistical analysis plans. Our biostatisticians possess the critical thinking to assist clients.</p><br /><br />
+<p>Our biostatisticians are industry experts in several statistical areas including adaptive design, personalized medicine, Bayesian analysis and non-inferiority approaches with extensive experience in several indications. Our biostatisticians have over 15 years of experience in NDA/BLA/PMA submission and interacting with FDA/EMEA and PMDA and actively collaborate with academic, industry and FDA working groups. Additionally, they are experienced with CDISC data standards and electronic submission standards to deliver high quality, dependable analyses.</p><br /><br />
+<p>Our extensive experience allows us to develop a statistically rigorous and customized plan for each project. Our execution of the data analysis plan produces report ready summary tables, data listings and figures for interpretation and inclusion in the study report or submission. A thorough parallel validation and senior review process substantiates the accuracy and completeness of all statistical analyses.</p><br /><br /><br /><br /><br /><br />;
+	document.getElementById("side_matter").innerHtml= "";
+	var butt=document.getElementById("read");
+	butt.parentNode.removeChild(butt);
+}
